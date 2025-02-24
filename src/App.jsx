8f2fae7d60aee781595638f4b6bd07d2c8b61116
ravindersingh74123 <154728708+ravindersingh74123@users.noreply.github.com>
2025-02-24 21:33:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ const mediaFiles = {
   "01-25": "/content/meme0125.mp4",
   "01-26": "/content/meme0126.mp4",
   "01-27": "/content/meme0127.jpg",
-  "01-28": "/content/meme0128.mp4",
+  "01-28": "/content/meme0128.jpg",
   "01-29": "/content/meme0129.mp4",
   "01-30": "/content/meme0130.mp4",
   "01-31": "/content/meme0131.mp4",
